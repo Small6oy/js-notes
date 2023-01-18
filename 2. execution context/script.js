e88550971e55426710a644a96f64eb93c7f9eef6
Small6oy <gggndlovu@gmail.com>
2023-01-18 21:58:17 +0200

    added notes

--- 2. execution context/script.js
+++ 2. execution context/script.js
@@ -1,14 +1,8 @@
-
-function first(){
-    console.log("first")
-    second()
+var x = 100
+var y = 50
+function getSum(n1, n2) {
+    var sum = n1 + n2
+    return sum
 }
-function second(){
-    console.log("second")
-    third()
-}
-function third(){
-    console.log("third")
-}
-
-first()
+var sum1 = getSum(x, y)
+var sum2 = getSum(10, 5)
